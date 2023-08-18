c2cec87b88a5a69c159c36177703e3fc59043680
uniboi <kami.0.katze@gmail.com>
2023-08-18 17:23:13 +0200

    server arbitrary calls test

--- src/sqr_compiler/main.rs
+++ src/sqr_compiler/main.rs
@@ -1,17 +1,23 @@
-extern crate dlopen;
-#[macro_use]
-extern crate dlopen_derive;
-use dlopen::{
-    raw::{AddressInfoObtainer, Library},
-    wrapper::WrapperApi,
-};
+#![allow(non_snake_case)]
 
-// TODO add relevant squirrel functions from server.dll
-#[derive(WrapperApi)]
-struct Api<'a> {
-    example_rust_fun: fn(arg: i32) -> u32,
-    example_c_fun: unsafe extern "C" fn(),
-    example_reference: &'a mut i32,
+use libloading::os::windows::Library;
+
+const TIER_0_PATH: &str = "/home/joe/dev/sqr_lsp/target/x86_64-pc-windows-gnu/debug/tier0.dll";
+const VSTDLIB_PATH: &str =
+    "/home/joe/.steam/steam/steamapps/common/Titanfall2/bin/x64_retail/vstdlib.dll";
+const SERVER_PATH: &str = "/home/joe/.steam/steam/steamapps/common/Titanfall2/server.dll";
+
+/// cast any arbitrary address to type `t`
+macro_rules! cast {
+    ($address: expr, $t: ty) => {
+        std::mem::transmute::<*const (), $t>($address as _)
+    };
+}
+
+#[derive(Debug)]
+struct RecordedAnimation {
+    _gap: [u8; 814],
+    refcount: u16,
 }
 
 fn main() {
@@ -20,20 +26,22 @@ fn main() {
     unsafe {
         let m = libc::malloc(128);
         println!("{}", libc::_msize(m));
-    }
+        let tier0 = Library::new(TIER_0_PATH).unwrap();
+        let vstdlib = Library::new(VSTDLIB_PATH).unwrap();
+        let server = Library::new(SERVER_PATH).unwrap();
 
-    let tier0_lib =
-        Library::open("/home/joe/dev/sqr_lsp/target/x86_64-pc-windows-gnu/debug/tier0.dll")
-            .unwrap();
-    let vstdlib_lib = Library::open(
-        "/home/joe/.steam/steam/steamapps/common/Titanfall2/bin/x64_retail/vstdlib.dll",
-    )
-    .unwrap();
-    return; // just test vstdlib.dll loading for now, shitter refuses to work
-    let lib =
-        Library::open("/home/joe/.steam/steam/steamapps/common/Titanfall2/server.dll").unwrap();
-    let ptr: *const i32 = unsafe { lib.symbol("entry").unwrap() };
-    let aio = AddressInfoObtainer::new();
-    let addr_info = aio.obtain(ptr as *const ()).unwrap();
-    println!("Library base address: {:?}", addr_info.dll_base_addr);
+        let server_handle = server.into_raw();
+        println!("server.dll base address: {}", server_handle);
+
+        let create_recorded_animation_ptr = (server_handle + 0x996e0) as *const ();
+        let create_recorded_animation: extern "C" fn() -> &'static RecordedAnimation =
+            std::mem::transmute(create_recorded_animation_ptr);
+
+        let add_anim_refcount = cast!(server_handle + 0x996c0, extern "C" fn(&RecordedAnimation));
+
+        let anim = create_recorded_animation();
+        add_anim_refcount(anim);
+
+        println!("anim ptr: {:?}, refcount is {}", anim, anim.refcount);
+    }
 }
